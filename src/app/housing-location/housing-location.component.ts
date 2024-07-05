07cf438b426c9d221af6ab7749c2065ba2066d52
furkan aydin <furknn2112@gmail.com>
2024-07-05 16:21:33 +0300

    Modify HousingLocation's template

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,11 +6,14 @@ import { HousingLocation } from '../housing-location';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="{{housingLocation.name}}" />
+    <h2 class="listing-heading">{{housingLocation.name}}</h2>
+    <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+  </section>
   `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
