f95776262289b59a7bf7f17334c0467b776a5474
furkan aydin <furknn2112@gmail.com>
2024-07-07 14:30:45 +0300

    ui: Update details-component's template and style

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -8,9 +8,25 @@ import { HousingService } from '../housing.service';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      details works!
-    </p>
+    <article>
+    <img class="listing-photo" [src]="housingLocation?.photo" [alt]="housingLocation?.name">
+    <section class="listing-description">
+      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+    </section>
+    <section class="listing-feature">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Unit available: {{housingLocation?.availableUnits}}</li>
+        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+        <li>Does this location have laundy: {{housingLocation?.laundry}}</li>
+      </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <button class="primary" type="button">Apply now</button>
+    </section>
+  </article>
   `,
   styleUrl: './details.component.css'
 })
