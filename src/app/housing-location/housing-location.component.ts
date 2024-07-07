aa474d6b31e6e53454b6364bdb2349bffc135f32
furkan aydin <furknn2112@gmail.com>
2024-07-07 13:32:44 +0300

    feat: Add hyperlink to link from housing-location-component to details-component
    - Import RouterLink to add the route for details-component
    - Use property binding to create dynamically route pattern

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housing-location';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   template: `
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="{{housingLocation.name}}" />
     <h2 class="listing-heading">{{housingLocation.name}}</h2>
     <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+    <a [routerLink]="['/details',housingLocation.id]">Get details</a>
   </section>
   `,
   styleUrl: './housing-location.component.css'
