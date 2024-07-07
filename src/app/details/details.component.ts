7d1661342a2f9acb60d84dc8d108d01af3821253
furkan aydin <furknn2112@gmail.com>
2024-07-07 11:45:55 +0300

    feat: Add route and housing service integration to DetailsComponent
    - Add ActivatedRoute to subscribe to route parameters
    - Add HousingService to fetch housing location by ID
    - Update DetailsComponent to handle housing location data

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-details',
@@ -12,5 +15,14 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  private housingLocationId!: number;
+  housingLocation: HousingLocation | undefined;
 
+  constructor(private route: ActivatedRoute,private housingService:HousingService) {
+     route.params.subscribe(params => {
+      this.housingLocationId = params['id']
+    });
+
+    this.housingLocation = housingService.getHousingLocationById(Number(this.housingLocationId));
+  }
 }
