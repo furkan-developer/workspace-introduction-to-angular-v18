a4584855d55e57834fdc79b420e81e52a8c41248
furkan aydin <furknn2112@gmail.com>
2024-07-09 23:26:45 +0300

    feat(component): Add validation rules to apply form
    - Add validation rules to apply model
    - Render all input's validation error in component's template individually

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -28,12 +28,36 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angul
       <form [formGroup]="applyForm" (submit)="onSubmit()">
         <label for="firstName">FirstName:</label>
         <input id="firstName" type="text" formControlName="firstName">
+         @if (applyForm.controls['firstName'].invalid && (applyForm.get('firstName')?.dirty || applyForm.get('firstName')?.touched)) {
+          @if (applyForm.controls['firstName'].hasError('required')) {
+            <div>First name is required</div>
+          }
+          @else if(applyForm.controls['firstName'].hasError('minlength')) {
+            <div> First name length must be at least 3 characters</div>
+          }
+        }
 
         <label for="lastName">LastName:</label>
         <input id="lastName" type="text" formControlName="lastName">
+        @if (applyForm.controls['lastName'].invalid && (applyForm.get('lastName')?.dirty || applyForm.get('lastName')?.touched)) {
+          @if (applyForm.controls['lastName'].hasError('required')) {
+            <div>Last name is required</div>
+          }
+          @else if(applyForm.controls['lastName'].hasError('minlength')) {
+            <div>Last name length must be at least 3 characters</div>
+          }
+        }
 
         <label for="email">Email:</label>
         <input id="email" type="email" formControlName="email">
+         @if (applyForm.controls['email'].invalid && (applyForm.get('email')?.dirty || applyForm.get('email')?.touched)) {
+          @if (applyForm.controls['email'].hasError('required')) {
+            <div>Email is required</div>
+          }
+          @else if(applyForm.controls['email'].hasError('email')) {
+            <div>Email is invalid</div>
+          }
+        }
         <button type="submit" class="primary">Apply now</button>
       </form>
     </section>
@@ -65,9 +89,9 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.applyForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
@@ -75,6 +99,7 @@ export class DetailsComponent implements OnInit {
     // TODO: handle datas inside applyFrom that is represent state of form
 
     console.log({
+      isValidForm: this.applyForm.valid,
       firstName: this.applyForm.value.firstName ?? '',
       lastName: this.applyForm.value.lastName ?? '',
       email: this.applyForm.value.email ?? ''
