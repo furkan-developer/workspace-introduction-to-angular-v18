85342b2b47c013302602e5fb43e8dab9e9858fb7
furkan aydin <furknn2112@gmail.com>
2024-08-11 16:48:49 +0300

    feat(home.component): Add filtering house functionality
    - Create onSearch method to filter hosuingLogication data by city
    - Bind to change event of input element along with onSearch method
    - Access to input element using ElementRef to read filtering text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { HousingLocation } from '../housing-location';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingService } from '../housing.service';
@@ -9,29 +9,52 @@ import { HousingService } from '../housing.service';
   imports: [HousingLocationComponent],
   template: `
     <section>
-      <input type="text" placeholder="Filter by ciy" />
+      <input
+        #cityFiltering
+        (change)="onSearch()"
+        type="text"
+        placeholder="Filter by ciy"
+      />
       <button type="button">Search</button>
     </section>
     <section class="results">
-      @for (item of housingLocations; track item) {
+      @if (filteredHousingLocation.length === 0) {
+      <h2>There is no house</h2>
+      } @else{ @for (item of filteredHousingLocation; track item) {
       <app-housing-location [housingLocation]="item"></app-housing-location>
-      }
+      } }
     </section>
   `,
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  housingLocations!: HousingLocation[];
+  housingLocations: HousingLocation[] = [];
+  filteredHousingLocation: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+
+  @ViewChild('cityFiltering') cityFiltering!: ElementRef;
 
   constructor() {
     this.housingService.getAllHousingLocations().subscribe({
       next: (data) => {
-        this.housingLocations = data;
+        this.housingLocations = this.filteredHousingLocation = data;
       },
       error: (error) => {
         console.error('Error fetching housing location', error);
       },
     });
+  }
+
+  onSearch() {
+    let nativeElement = this.cityFiltering.nativeElement;
+
+    if (
+      nativeElement.tagName.toLowerCase() === 'input' &&
+      nativeElement.getAttribute('type').toLowerCase() === 'text'
+    ) {
+      this.filteredHousingLocation = this.housingLocations.filter((hl) =>
+        hl.city.toLowerCase().includes(nativeElement.value.toLowerCase())
+      );
+    }
   }
 }
